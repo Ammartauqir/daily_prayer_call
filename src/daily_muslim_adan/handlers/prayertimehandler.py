8225176d09eeaf0b1aec6b428791af9e4467f43b
AmmarTauqir <ammar.tauqir2@gmail.com>
2024-03-10 11:55:47 +0100

    relocatig data files and fixing prayer time handler class

---
+++ src/daily_muslim_adan/handlers/prayertimehandler.py
@@ -0,0 +1,85 @@
+import requests
+import json
+from datetime import datetime
+import os
+from os.path import exists
+
+PRAYER_NAMES = ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha']  # skipping 'Fajr' as adan only rings 30min before sunrise
+PRAYER_FILE_PATH = "data\prayertime_files"
+TIME_FORMAT = "%H:%M"
+
+
+def replace_sunrise_with_wakeup_time(prayer_times_list, WAKEUP_TIME_BEFORE_SUNRISE):
+    wakeup_time = datetime.strptime(prayer_times_list[0], TIME_FORMAT) - datetime.strptime(
+        WAKEUP_TIME_BEFORE_SUNRISE,
+        TIME_FORMAT)
+    wakeup_time = str(wakeup_time)[:-3]
+    prayer_times_list[0] = wakeup_time
+    return prayer_times_list
+
+
+class PrayerTimeHandler:
+    def __init__(self, city, country, year):
+        self.annual_prayer_data = None
+        self.city = city
+        self.country = country
+        self.year = year
+        self.timings_filename = os.path.join(PRAYER_FILE_PATH, f"annual_prayer_times_{city}_{year}.json")
+        self.get_annual_prayer_times()
+
+    def download_annual_prayer_calander(self):
+        api_url = f'https://api.aladhan.com/v1/calendarByCity?city={self.city}&country={self.country}&method=1&annual=true&year={self.year}'
+        response_API = requests.get(api_url)
+        print(response_API.status_code)
+        data = response_API.json()
+        with open(self.timings_filename, "w") as write:
+            json.dump(data, write)
+
+    def get_annual_prayer_times(self):
+        if not exists(self.timings_filename):
+            self.download_annual_prayer_calander()
+        with open(self.timings_filename, 'r') as file:
+            self.annual_prayer_data = json.load(file)
+
+    def today_prayer_times(self, current_date):
+        month = current_date.month
+        day = current_date.day
+        today_times = []
+        for prayer in PRAYER_NAMES:
+            time = self.annual_prayer_data["data"][str(month)][day]["timings"][prayer]
+            time = time.split()[0]
+            today_times.append(time)
+        return today_times
+
+
+def get_current_time():
+    now = datetime.now()
+    current_time = now.strftime(TIME_FORMAT)
+    return current_time
+
+
+def get_prayer_times(city_name):
+    url = f"https://dailyprayer.abdulrcs.repl.co/api/{city_name}"
+    response = requests.get(url)
+    data = response.json()
+    # print(data['city'])
+    # print(data['date'])
+    prayer_time_list = []
+    for prayer in data["today"]:
+        prayer_time_list.append(data["today"][prayer])
+        # print(prayer + ": " + data["today"][prayer])
+    prayer_time_list = prayer_time_list[1:]  # removing Fajar start time
+    return prayer_time_list
+    # If you want to request for tomorrow prayer's time:
+    # for prayer in data["tomorrow"]:
+    #  print(prayer + ": " + data["tomorrow"][prayer])
+
+
+def get_prayer_time_diff(prayer_times_list, current_time):
+    prayer_diff_list = []
+    for time in prayer_times_list:
+        prayer_time = datetime.strptime(time, TIME_FORMAT)
+        current_time_dt = datetime.strptime(current_time, TIME_FORMAT)
+        diff = prayer_time - current_time_dt
+        prayer_diff_list.append(str(diff))
+    return prayer_diff_list
--- src/daily_muslim_adan/main.py
+++ src/daily_muslim_adan/main.py
@@ -3,70 +3,39 @@ from datetime import datetime
 import re
 import time
 import playsound
-from prayertimehandler import PrayerTimeHandler
-from audiofilehandler import play_audio
+from handlers.prayertimehandler import \
+    PrayerTimeHandler, \
+    get_current_time, \
+    get_prayer_times, \
+    get_prayer_time_diff
+from handlers.audiofilehandler import play_audio
 
-TIME_FORMAT = "%H:%M"
 WAKEUP_TIME_BEFORE_SUNRISE = "00:30"
 REGEX_MATCH = "0:00:00"
 
 
-def get_current_time():
-    now = datetime.now()
-    current_time = now.strftime(TIME_FORMAT)
-    return current_time
-
-
-def get_prayer_times(city_name):
-    url = f"https://dailyprayer.abdulrcs.repl.co/api/{city_name}"
-    response = requests.get(url)
-    data = response.json()
-    # print(data['city'])
-    # print(data['date'])
-    prayer_time_list = []
-    for prayer in data["today"]:
-        prayer_time_list.append(data["today"][prayer])
-        # print(prayer + ": " + data["today"][prayer])
-    prayer_time_list = prayer_time_list[1:]  # removing Fajar start time
-    return prayer_time_list
-    # If you want to request for tomorrow prayer's time:
-    # for prayer in data["tomorrow"]:
-    #  print(prayer + ": " + data["tomorrow"][prayer])
-
-
-def get_prayer_time_diff(prayer_times_list, current_time):
-    prayer_diff_list = []
-    for time in prayer_times_list:
-        prayer_time = datetime.strptime(time, TIME_FORMAT)
-        current_time_dt = datetime.strptime(current_time, TIME_FORMAT)
-        diff = prayer_time - current_time_dt
-        prayer_diff_list.append(str(diff))
-    return prayer_diff_list
-
-
 def main():
-    city = "Ingolstadt"
+    city  = "Ingolstadt"
     country = "Germany"
+    play_audio("adan.mp3")
+    print("xxxxxxxxxxxxx")
+    current_year = datetime.today().year
+    prayer_time_obj = PrayerTimeHandler(city, country, current_year)
     while True:
         current_datetime = datetime.today()
-        prayer_time_obj = PrayerTimeHandler(city, country, current_datetime.year)
-        prayer_time_dict = prayer_time_obj.get_annual_prayer_times()
-        prayer_times_list = prayer_time_obj.today_prayer_times(prayer_time_dict, current_datetime)
-        # prayer_times_list = get_prayer_times("ingolstadt")
+        prayer_times_list = prayer_time_obj.today_prayer_times(current_datetime)
         current_time = get_current_time()
-        prayer_times_list = prayer_time_obj.replace_sunrise_with_wakeup_time(prayer_times_list,
-                                                                             WAKEUP_TIME_BEFORE_SUNRISE)
+        # prayer_times_list = prayer_time_obj.replace_sunrise_with_wakeup_time(prayer_times_list,
+        #                                                                      WAKEUP_TIME_BEFORE_SUNRISE)
         prayer_time_diff = get_prayer_time_diff(prayer_times_list, current_time)
         print(f'_________________Current datetime : {current_datetime}_________________')
         for pr_time in prayer_time_diff:
             if re.match(REGEX_MATCH, pr_time):
                 print("_______prayer_________")
-                play_audio("")
-                playsound.playsound("adan.mp3")
-                time.sleep(120)
+                play_audio("adan.mp3")
             else:
                 print(pr_time)
-        time.sleep(59)
+        time.sleep(28)
 
 
 if __name__ == "__main__":
